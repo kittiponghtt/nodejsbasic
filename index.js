bcb2e111cf7e0a2aa1e40894714137dbb87a674e
kittipong hanwattanasiri <kittiponghtt@hotmail.com>
2023-02-05 12:48:03 +0700

    make route index.js

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
 const homepage = fs.readFileSync(`${__dirname}/templates/index.html`)
-const productpage = fs.readFileSync(`${__dirname}/templates/product1.html`)
+const productpage1 = fs.readFileSync(`${__dirname}/templates/product1.html`,'utf-8')
+const productpage2 = fs.readFileSync(`${__dirname}/templates/product2.html`,'utf-8')
+const productpage3 = fs.readFileSync(`${__dirname}/templates/product3.html`,'utf-8')
 http.createServer((req,res)=>{
-const pathName = req.url;
-if(pathName==="/home" || pathName ==='/'){
-    res.end(homepage)
-}else if(pathName==='/product'){
-    res.end(productpage)
+    const {pathname,query} = (url.parse(req.url,true))
+   
+    if(pathname==="/home" || pathname ==='/'){
+        res.end(homepage)
+    }else if(pathname==='/product'){
+        if(query.id==="1"){
+            res.end(productpage1)
+        }else if(query.id==="2"){
+            res.end(productpage2)
+        }else{
+            res.end(productpage3)
+        }
+       
 
-}else{
-    res.writeHead(404)
-    res.end("Not found")
-}
+    }else{
+        res.writeHead(404)
+        res.end("Not found")
+    }
 
    
 }).listen(3000,'localhost',()=>{
